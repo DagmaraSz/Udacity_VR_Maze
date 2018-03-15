f368b77de0ca0017eb8588d334cf9e7191e4e46a
DagmaraSz <daga.sz@hotmail.com>
2018-03-15 17:08:41 +0000

    Created a coin and made it poof

--- Assets/UdacityVR/Scripts/Coin.cs
+++ Assets/UdacityVR/Scripts/Coin.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject coinPoofPrefab;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		GameObject coinPoof = (GameObject) Instantiate(coinPoofPrefab, transform.position, transform.rotation);
         // Make sure the poof animates vertically
+
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Destroy(gameObject);
     }
 
 }
