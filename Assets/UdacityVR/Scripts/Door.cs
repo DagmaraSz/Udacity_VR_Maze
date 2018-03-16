6c9c8093b9f7bf13d6e00985556dcd57a161979c
DagmaraSz <daga.sz@hotmail.com>
2018-03-16 13:08:30 +0000

    door opens and closes with sounds

--- Assets/UdacityVR/Scripts/Door.cs
+++ Assets/UdacityVR/Scripts/Door.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class Door : MonoBehaviour 
 {
-    // Create a boolean value called "locked" that can be checked in OnDoorClicked() 
+	private AudioSource _doorSounds;
+	public AudioClip doorClosedSound;
+	public AudioClip doorOpenSound;
+
+	// Create a boolean value called "locked" that can be checked in OnDoorClicked() 
     // Create a boolean value called "opening" that can be checked in Update() 
+	bool locked = true, opening = false;
+
+	void Start() {
+		_doorSounds = GetComponent<AudioSource> ();
+	}
 
     void Update() {
         // If the door is opening and it is not fully raised
-            // Animate the door raising up
+		if (opening && transform.position.y < 7.5f) {
+			// Animate the door raising up
+			transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
+		}
     }
 
     public void OnDoorClicked() {
         // If the door is clicked and unlocked
-            // Set the "opening" boolean to true
+		if (!locked) {
+			// Set the "opening" boolean to true
+			opening = true;
+			_doorSounds.PlayOneShot (doorOpenSound, 1);
         // (optionally) Else
+		}
+		else {
             // Play a sound to indicate the door is locked
+			_doorSounds.PlayOneShot (doorClosedSound, 1);
+		}
     }
 
     public void Unlock()
     {
         // You'll need to set "locked" to false here
+			locked = false;
     }
 }
